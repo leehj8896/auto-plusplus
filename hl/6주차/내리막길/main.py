d4f91284d60482e205e80cd1b08c4363b424b190
leehj8896 <leehj8896@gmail.com>
2022-10-23 21:54:07 +0900

    hl 6주차 내리막길

--- hl/6주차/내리막길/main.py
+++ hl/6주차/내리막길/main.py
@@ -1,9 +1,7 @@
-import sys
-sys.setrecursionlimit(10000)
-
 h, w = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(h)]
 dy, dx = [-1,0,1,0], [0,1,0,-1]
+
 count = [[-1]*w for _ in range(h)]
 
 def dfs(cy, cx):
@@ -11,22 +9,24 @@ def dfs(cy, cx):
     if cy == h-1 and cx == w-1:
         return 1
     
-    if count[cy][cx] == 0:
-        count[cy][cx] = 0
+    # 이미 갔던 위치이면
+    if count[cy][cx] != -1:
+        return count[cy][cx]
+    
+    count[cy][cx] = 0
 
     for i in range(4):
         ny = cy + dy[i]
         nx = cx + dx[i]
         if 0 <= ny < h and 0 <= nx < w:
             if board[cy][cx] > board[ny][nx]:
-                count[ny][nx] += dfs(ny, nx)
+                count[cy][cx] += dfs(ny, nx)
     
     return count[cy][cx]
 
 
 dfs(0,0)
-print(count[-1][-1])
-pass
+print(count[0][0])
 
 '''
 4 5
