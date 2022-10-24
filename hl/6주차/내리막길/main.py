d0f0cde4418c333e505882c689979713d25bba8d
leehj8896 <leehj8896@gmail.com>
2022-10-24 02:04:05 +0000

    hl 6th downhill modified

--- hl/6주차/내리막길/main.py
+++ hl/6주차/내리막길/main.py
@@ -1,38 +1,43 @@
-h, w = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(h)]
+#h, w = map(int, input().split())
+#board = [list(map(int, input().split())) for _ in range(h)]
+#'''
+h, w = [4,5]
+board = [
+  [50,45,37,32,30],
+  [35,50,40,20,25],
+  [30,30,25,17,24],
+  [27,24,22,15,10],
+]
+#'''
+
 dy, dx = [-1,0,1,0], [0,1,0,-1]
 
-count = [[-1]*w for _ in range(h)]
+visited = [[0]*w for _ in range(h)]
+count = [[0]*w for _ in range(h)]
 
 def dfs(cy, cx):
-
+    
     if cy == h-1 and cx == w-1:
+        count[cy][cx] = 1 
         return 1
     
-    # 이미 갔던 위치이면
-    if count[cy][cx] != -1:
-        return count[cy][cx]
-    
-    count[cy][cx] = 0
-
     for i in range(4):
         ny = cy + dy[i]
         nx = cx + dx[i]
         if 0 <= ny < h and 0 <= nx < w:
             if board[cy][cx] > board[ny][nx]:
-                count[cy][cx] += dfs(ny, nx)
+                if not visited[ny][nx]:
+                    print(ny, nx, board[ny][nx])
+                    visited[ny][nx] = 1
+                    count[cy][cx] += dfs(ny, nx)
+                else:
+                    count[cy][cx] += count[ny][nx]
     
     return count[cy][cx]
 
-
+visited[0][0] = 1
 dfs(0,0)
 print(count[0][0])
 
-'''
-4 5
-50 45 37 32 30
-35 50 40 20 25
-30 30 25 17 28
-27 24 22 15 10
-
-'''
+for row in count:
+    print(row)
